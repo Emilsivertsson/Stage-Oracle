b037efc5e0aa2f4fb26f61fb0988509bee69be19
Emilsivertsson <Emil.sivertsson@gmail.com>
2024-04-11 10:29:29 +0200

    workflow test 2 with dockerhub

--- registration-service/src/test/java/org/codeforpizza/registrationservice/RegistrationServiceApplicationTests.java
+++ registration-service/src/test/java/org/codeforpizza/registrationservice/RegistrationServiceApplicationTests.java
@@ -1,13 +1,26 @@
 package org.codeforpizza.registrationservice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest
+@TestConfiguration(proxyBeanMethods = false)
 class RegistrationServiceApplicationTests {
 
-    @Test
-    void contextLoads() {
+    @Bean
+    @ServiceConnection
+    MySQLContainer<?> mysqlContainer() {
+        return new MySQLContainer<>(DockerImageName.parse("mysql:latest"));
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.from(RegistrationServiceApplicationTests::main).with(RegistrationServiceApplicationTests.class).run(args);
     }
 
 }
