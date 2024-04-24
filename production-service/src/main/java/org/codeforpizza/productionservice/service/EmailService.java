9100be54efb1487774726b46ad3b5119b134253d
Emilsivertsson <Emil.sivertsson@gmail.com>
2024-04-24 13:34:41 +0200

    updated Docker-compose.yaml

--- production-service/src/main/java/org/codeforpizza/productionservice/service/EmailService.java
+++ production-service/src/main/java/org/codeforpizza/productionservice/service/EmailService.java
@@ -2,6 +2,8 @@ package org.codeforpizza.productionservice.service;
 
 import lombok.RequiredArgsConstructor;
 import org.codeforpizza.productionservice.modell.DTOs.Email;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mail.SimpleMailMessage;
@@ -12,12 +14,14 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class EmailService {
 
+    private static final Logger log = LoggerFactory.getLogger(EmailService.class);
     @Value("${email.from}")
     private String fromAdress;
 
     private final JavaMailSender javaMailSender;
 
     public ResponseEntity<String> sendEmail(Email email) {
+        try{
         SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
         simpleMailMessage.setFrom(fromAdress);
         simpleMailMessage.setTo(email.adress());
@@ -25,5 +29,9 @@ public class EmailService {
         simpleMailMessage.setText(email.body() + "\n This is sent from Stage Oracle Application, You cant answer this email. " );
         javaMailSender.send(simpleMailMessage);
         return ResponseEntity.ok("Email sent");
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return ResponseEntity.badRequest().build();
+        }
     }
 }
